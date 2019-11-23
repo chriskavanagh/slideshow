84e22dfd842528ca596748c0b8be99829ff3f3fd
Chris Kavanagh <ckava3@gmail.com>
2019-11-23 08:49:02 -0500

    css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,12 @@ const Nav = styled.nav`
   height: 10vh;
   background: #5b78c7;
 
+  a {
+    color: white;
+    text-decoration: none;
+    font-size: 16px;
+  }
+
   @media screen and (max-width: 768px) {
     position: relative;
   }
@@ -57,15 +63,32 @@ const UL = styled.ul`
     pointer-events: none;
 
     ${({ open }) => {
-      if (open === true) return `clip-path: circle(1500px at 90% -10%)`;
+      if (open === true) return `clip-path: circle(1200px at 90% -10%)`;
       if (open === true) return `pointer-events: all`;
     }}
   }
 `;
 
+const StyledSection = styled.section`
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    margin: 100px;
+    font-size: 50px;
+    color: #ae5fce;
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
 const Navbar = props => {
-  console.log(props);
   const [open, setOpen] = useState(false);
+  console.log(open);
 
   const toggleCollapse = () => {
     setOpen(!open);
@@ -83,17 +106,17 @@ const Navbar = props => {
         </Hamburger>
         <UL open={open}>
           {pages.map((page, i) => (
-            <li className={open ? "fade" : ""} key={i}>
+            <li className={open ? "fade" : "non--fade"} key={i}>
               <a href={page.toLowerCase()}>{page.split("/")}</a>
             </li>
           ))}
         </UL>
       </Nav>
 
-      <section className="landing">
+      <StyledSection className="landing">
         <img src={circles} alt="dots" />
         <h1>Dots</h1>
-      </section>
+      </StyledSection>
     </>
   );
 };
